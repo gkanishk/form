76fdde718a1e6dd960d1295e5ba89fa9edbfede1
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-06-21 20:51:11 +0530

    Styled stage 1

--- src/components/showData.js
+++ src/components/showData.js
@@ -1,15 +1,39 @@
 import React from 'react'
 
 function Showdata(props) {
+    console.log(props)
     return (
-        <div style={{backgroundColor:'#EFF3FE',opacity:'60%',width:'500px',marginLeft:'350px',padding:'20px',marginTop:'20px',borderRadius:'25px'}}>
-            <p>First Name: {props.data.first}</p>
-            <p>Last Name: {props.data.last}</p>
-            <p>Gender {props.data.gender}</p>
-            <p>Email: {props.data.email}</p>
-            <p>Age: {props.data.age}</p>
-            <p>Marital Status: {props.data.status}</p>
-            <p>Qualification:{props.data.qualify}</p>
+        <div style={{backgroundColor:'#EFF3FE',opacity:'60%',width:'400px',marginLeft:'400px',padding:'20px',marginTop:'20px',borderRadius:'25px'}}>
+        <div style={{textAlign:'start'}}>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>First Name:</div>
+                <div style={{flexGrow:'2'}}>{props.data.first}</div>
+            </div>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>Last Name:</div>
+                <div style={{flexGrow:'2'}}>{props.data.last}</div>
+            </div>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>Gender:</div>
+                <div style={{flexGrow:'2'}}>{props.data.gender}</div>
+            </div>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>Email:</div>
+                <div style={{flexGrow:'2'}}>{props.data.email}</div>
+            </div>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>Age:</div>
+                <div style={{flexGrow:'2'}}>{props.data.age}</div>
+            </div>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>Marital Status:</div>
+                <div style={{flexGrow:'2'}}>{props.data.status}</div>
+            </div>
+            <div style={{display:'flex',justifyContent:'flex-between'}}>
+                <div style={{width:'110px'}}>Qualification:</div>
+                <div style={{flexGrow:'2'}}>{props.data.qualify}</div>
+            </div>
+        </div>
         </div>
     )
 }
