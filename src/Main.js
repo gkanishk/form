c2840748f46d5b53d8a971d4805c16fa805dc3da
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-08-15 20:01:18 +0530

    added main

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Main() {
     return (
         <div>
-            Main page
+            <h2>Main page</h2>
+            <nav>
+            <ul>
+                <li>
+                <Link to="/user">User</Link>
+                </li>
+                <li>
+                <Link to="/admin">Admin</Link>
+                </li>
+            </ul>
+            </nav>
         </div>
     )
 }
